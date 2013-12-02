6b4fbdd2df3d9678dd74a8aa138ce00f77147448
jasonlarkin <jasonlarkin84@gmail.com>
2013-12-02 10:01:15 -0500

    added dsf code

---
+++ matlab/m_dsf_seed_avg.tmp.m
@@ -0,0 +1,34 @@
+NMD=load('./nmd.mat');
+
+[tmp,str.main]=system('pwd');
+
+%modes
+    SED.modemaster = NMD.modemaster;
+%Input SED
+    SED.sed( 1:NMD.NUM_TSTEPS/2,1:NMD.NUM_KPTS ) = 0.0;
+
+for ikpt = 1:NMD.NUM_KPTS
+%--------------------------------------------------------------------------
+tic
+%--------------------------------------------------------------------------
+        str.read=...
+            strcat(NMD.str.main,'nmd/SED_',...
+                num2str(NMD.modemaster(ikpt)),'.txt');
+        dummy = load(str.read); 
+        %sed
+        SED.sed(1:NMD.NUM_TSTEPS/2,ikpt) = dummy(:,2);
+        %omega
+        SED.omega = dummy(:,1);
+%--------------------------------------------------------------------------
+toc
+%--------------------------------------------------------------------------
+end
+
+save(...
+    strcat(...
+    NMD.str.main,'NMDavg.mat'), '-struct', 'NMD');
+save(...
+    strcat(...
+    NMD.str.main,'SEDavg.mat'), '-struct', 'SED');
+
+
